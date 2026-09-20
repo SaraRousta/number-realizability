cff7a8078f89b4c8e496298d0ebe403f39a0c3a1
SaraRousta <rousta.s@pm.me>
2026-09-20 06:16:38 +0200

    reordering

--- LogicalFacts.v
+++ LogicalFacts.v
@@ -2,6 +2,39 @@ From SyntheticComputability Require Import Definitions.
 
 (** * Logical Principles and Equivalences *)
 
+Definition DNE := forall (P : Prop), ~~ P -> P.
+
+Definition AC_nat :=  forall R : nat -> nat -> Prop, (forall x, exists y, R x y) -> exists f : nat -> nat, forall x, R x (f x).
+
+Definition MP_bool := 
+  forall (f : nat -> bool), 
+    (~~ exists n, f n = true) -> exists n, f n = true.
+
+Definition MP_decidable := 
+  forall (P : nat -> Prop), 
+    (decidable P) -> 
+      (~~ exists n : nat, P n) -> exists n : nat, P n.
+
+Lemma quant_dist_all_ex {X : Type} (P : X -> Prop) (Q : Prop) : 
+  (forall x, (P x -> Q)) <-> ((exists x, P x) -> Q).
+Proof.
+  split.
+  intros ? [].
+  eapply H; eassumption.
+  intros. apply H.
+  exists x. assumption.
+Qed.
+
+Lemma iff_neg (P Q : Prop) : (P <-> Q) -> (~P <-> ~Q).
+Proof.
+  intros H. intuition.
+Qed.
+
+Lemma false_not_true (f : nat -> bool) (n : nat) : f n = false -> ~(f n = true).
+Proof.
+  intuition. congruence.
+Qed.
+
 Lemma ex_P_and_False (A : Type) (P : A -> Prop) :
   (exists x, P x /\ False) <-> False.
 Proof.
@@ -14,38 +47,4 @@ Proof.
   split; intros H.
   - intros [x Hx]. apply (H x). assumption.
   - intros x Hx. apply H. exists x. assumption.
-Qed.
-
-Definition DNE := forall (P : Prop), ~~ P -> P.
-
-Definition MP_bool := 
-  forall (f : nat -> bool), 
-    (~~ exists n, f n = true) -> exists n, f n = true.
-
-Definition MP_decidable := 
-  forall (P : nat -> Prop), 
-    (decidable P) -> 
-      (~~ exists n : nat, P n) -> exists n : nat, P n.
-
-
-Lemma quant_dist_all_ex {X : Type} (P : X -> Prop) (Q : Prop) : 
-  (forall x, (P x -> Q)) <-> ((exists x, P x) -> Q).
-Proof.
-  split.
-  intros ? [].
-  eapply H; eassumption.
-  intros. apply H.
-  exists x. assumption.
-Qed.
-
-Definition AC_nat :=  forall R : nat -> nat -> Prop, (forall x, exists y, R x y) -> exists f : nat -> nat, forall x, R x (f x).
-
-Lemma iff_neg (P Q : Prop) : (P <-> Q) -> (~P <-> ~Q).
-Proof.
-  intros H. intuition.
-Qed.
-
-Lemma false_not_true (f : nat -> bool) (n : nat) : f n = false -> ~(f n = true).
-Proof.
-  intuition. congruence.
 Qed.
